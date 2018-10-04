cac4ecbd7ff77ca34a8588fc6af5a7ac401f87cf
Ian Hundere <ianfundere@gmail.com>
2018-10-04 15:59:02 -0400

    adds madlibs

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,48 @@
-// hard-coding instead of prompting
-// because in real life, when you want 
-// user input, you'll create an html form.
-
-// using backticks, hipster strings.
-// const name = `ian hundere`;
-// const subject = `having fun`;
-
-// hipster strings let you do linbreaks
-const finalText = `${name}'s favorite subject in school
-is ${subject}.;
-
-How lame.`;
-
-// optionally, you can use \n for linebreaks.
-// const finaText = `${name}'s 
-// favorite subject in school is $
-// {subject}.\n\nHow lame.`;
+// Start with hard-coded value
+// until you know your program works.
+// then switch to prompting the user.
 
 
-console.log(finalText);
+// const theDay = prompt("Day (0-6)?")
+let theDay = prompt(`Day (0-6)?`);
+
+// to convert from a string to a number,
+// use parseInt and provide base 10 as an argument
+theDay = parseInt(theDay, 10);
+
+// const theDay = 6;
+let dayName = `invalid choice, friend.`;
+
+// You must use paraentheses for your condition!
+// "Loose" equality is == 
+// "Strict" equality is ===
+// By default, use strict equality, ===
+// "threequals"
+if (theDay === 0) { // And curly braces!
+    // you can leave off the curly braces,
+    // but your "body" can only have one statement.
+    // any statement after the first will get run
+    // regardless of whether the condition is true or
+    // not
+    dayName = `Sunday`;
+} else if (theDay === 1) {
+    dayName = `Monday`;
+} else if (theDay === 2) {
+    dayName = `Tuesday`;
+} else if (theDay === 3) {
+    dayName = `Wednesday`;
+} else if (theDay === 4) {
+    dayName = `Thursday`;
+} else if (theDay === 5) {
+    dayName = `Friday`;
+} else if (theDay === 6) {
+    dayName = `Saturday`;
+} else if (theDay === 7) {
+    dayName = `Sunday`;
+}
+// The body of an if statement 
+// is surrounded by curly braces.
+// And does not require a semi-colon
+// after the curly braces.
+
+console.log(dayName);
